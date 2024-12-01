764bb5030f9e5defc7aa04237f246d04c360cd4d
Arthur <arthur.sampaio@upe.br>
2024-11-30 21:46:45 -0300

    feat: adicionando notblank

--- api-boleto/src/main/java/br/com/boleto/apiboleto/dto/BoletoRequestDTO.java
+++ api-boleto/src/main/java/br/com/boleto/apiboleto/dto/BoletoRequestDTO.java
@@ -1,7 +1,9 @@
 package br.com.boleto.apiboleto.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record BoletoRequestDTO(@NotNull(message = "O valor não pode ser nulo")
+public record BoletoRequestDTO(@NotNull(message = "valor não pode ser nulo")
+                               @NotBlank(message = "valor não pode ser nulo")
                                String codigoBarras){
 }
