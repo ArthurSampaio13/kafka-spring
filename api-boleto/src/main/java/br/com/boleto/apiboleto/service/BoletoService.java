3d445fb47708916cccb42a4a7d68a4357dd77ea2
Arthur <arthur.sampaio@upe.br>
2024-11-30 21:36:45 -0300

    feat: logica para salvar boleto

--- api-boleto/src/main/java/br/com/boleto/apiboleto/service/BoletoService.java
+++ api-boleto/src/main/java/br/com/boleto/apiboleto/service/BoletoService.java
@@ -1,4 +1,33 @@
 package br.com.boleto.apiboleto.service;
 
+import br.com.boleto.apiboleto.controller.exception.ApplicationException;
+import br.com.boleto.apiboleto.dto.BoletoDTO;
+import br.com.boleto.apiboleto.entity.BoletoEntity;
+import br.com.boleto.apiboleto.entity.enums.SituacaoBoleto;
+import br.com.boleto.apiboleto.mapper.BoletoMapper;
+import br.com.boleto.apiboleto.repository.BoletoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
 public class BoletoService {
+    private final BoletoRepository boletoRepository;
+
+    public BoletoDTO salvar(String codigoBarras) {
+        var boletoOptional = boletoRepository.findByCodigoBarras(codigoBarras);
+        if (boletoOptional.isPresent()) {
+            throw new ApplicationException("Já existe uma solicitação de pagamento para esse boleto");
+        }
+        var boletoEntity = BoletoEntity.builder()
+                .codigoBarras(codigoBarras)
+                .situacaoBoleto(SituacaoBoleto.INICIALIZADO)
+                .dataCriacao(LocalDateTime.now())
+                .dataAtualizacao(LocalDateTime.now())
+                .build();
+        boletoRepository.save(boletoEntity);
+        return BoletoMapper.toDTO(boletoEntity);
+    }
 }
