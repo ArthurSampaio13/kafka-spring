7c501a9af7d4d4f6f245bddc18c627392d91ba13
Arthur <arthur.sampaio@upe.br>
2024-11-30 21:35:24 -0300

    feat: criando rota para salvar um boleto

--- api-boleto/src/main/java/br/com/boleto/apiboleto/controller/BoletoController.java
+++ api-boleto/src/main/java/br/com/boleto/apiboleto/controller/BoletoController.java
@@ -1,4 +1,25 @@
 package br.com.boleto.apiboleto.controller;
 
+import br.com.boleto.apiboleto.dto.BoletoDTO;
+import br.com.boleto.apiboleto.dto.BoletoRequestDTO;
+import br.com.boleto.apiboleto.service.BoletoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/controller")
+@RequiredArgsConstructor
 public class BoletoController {
+    private final BoletoService boletoService;
+
+    @PostMapping
+    public ResponseEntity<BoletoDTO> salvar(@RequestBody BoletoRequestDTO boletoRequestDTO) {
+        var boleto = boletoService.salvar(boletoRequestDTO.codigoBarras());
+        return new ResponseEntity<>(boleto, HttpStatus.CREATED);
+    }
 }
