e4e21b5b2a623ede3e72eaf34fbace90bc2c206c
Arthur <arthur.sampaio@upe.br>
2024-11-30 21:36:24 -0300

    feat: repository do boleto

--- api-boleto/src/main/java/br/com/boleto/apiboleto/repository/BoletoRepository.java
+++ api-boleto/src/main/java/br/com/boleto/apiboleto/repository/BoletoRepository.java
@@ -1,4 +1,13 @@
 package br.com.boleto.apiboleto.repository;
 
-public class BoletoRepository {
+import br.com.boleto.apiboleto.entity.BoletoEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface BoletoRepository extends CrudRepository<BoletoEntity, UUID> {
+    Optional<BoletoEntity> findByCodigoBarras(String codigoBarras);
 }
