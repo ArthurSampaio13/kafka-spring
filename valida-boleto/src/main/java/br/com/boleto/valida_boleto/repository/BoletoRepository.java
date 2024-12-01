fad05a3764a0d905aaca150a91a76d6b9e8acb42
Arthur <arthur.sampaio@upe.br>
2024-12-01 16:23:08 -0300

    feat: boleto repository

--- valida-boleto/src/main/java/br/com/boleto/valida_boleto/repository/BoletoRepository.java
+++ valida-boleto/src/main/java/br/com/boleto/valida_boleto/repository/BoletoRepository.java
@@ -1,4 +1,11 @@
 package br.com.boleto.valida_boleto.repository;
 
-public interface BoletoRepository {
+import br.com.boleto.valida_boleto.entity.BoletoEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BoletoRepository extends CrudRepository<BoletoEntity, UUID> {
 }
