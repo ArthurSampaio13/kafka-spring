a988b47c493e12a3d7d9fe0b812fdefff620daf1
Arthur <arthur.sampaio@upe.br>
2024-11-30 21:36:07 -0300

    feat: mapper para o boletoEntity

--- api-boleto/src/main/java/br/com/boleto/apiboleto/mapper/BoletoMapper.java
+++ api-boleto/src/main/java/br/com/boleto/apiboleto/mapper/BoletoMapper.java
@@ -1,4 +1,15 @@
 package br.com.boleto.apiboleto.mapper;
 
+import br.com.boleto.apiboleto.dto.BoletoDTO;
+import br.com.boleto.apiboleto.entity.BoletoEntity;
+
 public class BoletoMapper {
+    public static BoletoDTO toDTO(BoletoEntity boletoEntity) {
+        return BoletoDTO.builder()
+                .situacaoBoleto(boletoEntity.getSituacaoBoleto())
+                .codigoBarras(boletoEntity.getCodigoBarras())
+                .dataCriacao(boletoEntity.getDataCriacao())
+                .dataAtualizacao(boletoEntity.getDataAtualizacao())
+                .build();
+    }
 }
