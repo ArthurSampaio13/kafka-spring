2018b9abcc2b227c75649a83379a4059af9b351c
Arthur <arthur.sampaio@upe.br>
2024-11-30 21:35:38 -0300

    feat: DTO de boleto

--- api-boleto/src/main/java/br/com/boleto/apiboleto/dto/BoletoDTO.java
+++ api-boleto/src/main/java/br/com/boleto/apiboleto/dto/BoletoDTO.java
@@ -1,4 +1,13 @@
 package br.com.boleto.apiboleto.dto;
 
-public class BoletoDTO {
+import br.com.boleto.apiboleto.entity.enums.SituacaoBoleto;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record BoletoDTO(String codigoBarras,
+                        SituacaoBoleto situacaoBoleto,
+                        LocalDateTime dataCriacao,
+                        LocalDateTime dataAtualizacao) {
 }
