6a793214980a1e31f9cd7b50a485a49e025f32a9
Arthur <arthur.sampaio@upe.br>
2024-12-01 22:05:22 -0300

    feat: logica para do boletoconsumer

--- valida-boleto/src/main/java/br/com/boleto/valida_boleto/service/kafka/BoletoConsumer.java
+++ valida-boleto/src/main/java/br/com/boleto/valida_boleto/service/kafka/BoletoConsumer.java
@@ -1,4 +1,21 @@
 package br.com.boleto.valida_boleto.service.kafka;
 
+import br.com.boleto.avro.Boleto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BoletoConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BoletoConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topico-boleto}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consumeBoleto(@Payload Boleto boleto) {
+        LOGGER.info("Consumindo mensagem -> {}", boleto);
+        System.out.println(boleto.toString());
+    }
+
 }
