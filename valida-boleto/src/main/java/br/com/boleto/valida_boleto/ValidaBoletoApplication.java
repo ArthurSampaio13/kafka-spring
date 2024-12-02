726b85694661c554ade411a1dbbf6d7f5e312250
Arthur <arthur.sampaio@upe.br>
2024-12-01 22:04:59 -0300

    feat: adicionando anotacao de enablekafka

--- valida-boleto/src/main/java/br/com/boleto/valida_boleto/ValidaBoletoApplication.java
+++ valida-boleto/src/main/java/br/com/boleto/valida_boleto/ValidaBoletoApplication.java
@@ -2,8 +2,10 @@ package br.com.boleto.valida_boleto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class ValidaBoletoApplication {
 
 	public static void main(String[] args) {
